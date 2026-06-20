4f15f2d58b6cadcedeb40eddb3062d185bca2b5d
alvvik <alvv2115@gmail.com>
2026-06-20 14:40:43 +0200

    added env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCxNT6oDy1l_6xOnaWPO4NMnPYzKCN_Tmw",
-  authDomain: "cinemaapp-55277.firebaseapp.com",
-  projectId: "cinemaapp-55277",
-  storageBucket: "cinemaapp-55277.firebasestorage.app",
-  messagingSenderId: "574979703214",
-  appId: "1:574979703214:web:b56defc81925e9c0b6083f",
-  measurementId: "G-EV8YK3M68Q",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
